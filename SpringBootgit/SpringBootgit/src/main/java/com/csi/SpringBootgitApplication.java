3412efdf0c9980c0ec10465609e8d5b903d1bb7a
Shital Bhagat <bhagatshital691999@gmail.com>
2023-01-19 12:05:37 +0530

    initial commit

---
+++ SpringBootgit/SpringBootgit/src/main/java/com/csi/SpringBootgitApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootgitApplication.class, args);
+	}
+
+}
